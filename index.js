f05730bea443980d2cd5a0599e1f50928bea2f78
aamandayk <amanda.freitas15@escola.pr.gov.br>
2026-06-22 08:54:30 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -30,19 +30,36 @@ app.post("/clientes", (req,res) => {
      app.get("/clientes", (req,res) => {
      try {
         const bd =  JSON.parse (fs.readFileSync ("bd.json", "utf8"))
-    req.status(200).json({resposta: bd})
+    res.status(200).json({resposta: bd})
     } catch (erro) {
          res.status (500).json({erro: erro.message})
 
       }}
+
       ) 
 
 app.get("/perfil", (req, res) => {
     res.json({nome : "Amandaaa" , idade : "15 anos"})
 })
 
+
+app.get("/clientes/:cpf", (req,res) => {
+    const cpf = req.params.cpf
+    try {
+        const bd = JSON.parse(fs.readFileSync("bd.json", "utf8"))
+        const cliente =  bd.find((cliente) => cliente.cpf == cpf)
+    if(!cliente){
+        return res.status(404).json({})
+    }
+        res.status (200).json({resposta: cliente})
+    } catch (erro){
+          res.status (500).json({erro: erro.message})
+}
+})
+
 app.listen(port, () => {
     console.log ("API executando na porta" + port)
 })
 
-//  GET http://localhost:3000/clientes*
+//  GET http://localhost:3000/clientes
+// TESTAR TODAS AS ROTAS: post, get geral e get cpf
